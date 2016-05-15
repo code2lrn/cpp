932193ffa5fe83af29feabd0b691651d78b420cd
Ranga <code2lrn@gmail.com>
2016-05-15 00:44:37 -0400

    Program to track FX ticker

--- hackerrank/currencymarket.cpp
+++ hackerrank/currencymarket.cpp
@@ -8,38 +8,31 @@
 #include <map>
 #include <algorithm>
 
+/*
+ * Logic:
+ *      The program will remember:
+ *      A. Only the LATEST Buy & Sell values for every Currency Pair submitted by each Bank
+ *      B. An up to date recollection of the best Buy & Sell rates per Currency Pair as of the last input record
+ *
+ *      For every input record received, these 4 steps will happen:
+ *      1. Parse input record in individual fields
+ *      2. Save the latest Bank=>( CCY Pair=>Buy & Sell ) mapping
+ *      3. Update
+ *          3.a Best Buy Price: CCY Pair => { Bank, Rate }
+ *          3.b Best Sell Price: CCY Pair => { Bank, Rate }
+ *      4. Printing the required output
+ */
+
 enum IncomingQuoteRecord {
     TIMESTAMPID = 0, CURRENCYPAIR, BANK, BUYRATE, SELLRATE, TOTALFIELDS
 };
-enum Quote {
-    BUY, SELL
-};
-using Bank    = std::string;
-using CcyPair = std::string;
 
-struct CcyPairQuote {
-    CcyPair pair;
-    double buyRate;
-    double sellRate;
-
-    CcyPairQuote() = default;
-
-    CcyPairQuote( const CcyPair &cp, const double &bR, const double &sR ) : pair( cp ), buyRate( bR ),
-                                                                            sellRate( sR ) { }
-};
-
-using LatestBankQuotes = std::map< Bank, std::map< CcyPair, CcyPairQuote > >;
-
-struct BankQuoteRate {
-    Bank bank;
-    Quote quote;
-    double price;
-
-    BankQuoteRate() = default;
-    BankQuoteRate( const Bank &b, const Quote &q, const double &p ) : bank( b ), quote( q ), price( p ) { }
-};
-
-using BestCcyQuotes = std::map< CcyPair, BankQuoteRate >;
+/*
+ * Class: QuoteManager
+ *
+ * The following class encapsulates the data structures & algorithm
+ * required to implement the above requirement.
+ */
 
 class QuoteManager {
 public:
@@ -48,30 +41,69 @@ public:
             return;
         }
 
+        // Step 1
         Bank bank = record[ IncomingQuoteRecord::BANK ];
         CcyPair ccyPair = record[ IncomingQuoteRecord::CURRENCYPAIR ];
         double buyRate = atof( record[ IncomingQuoteRecord::BUYRATE ].c_str() );
-        BankQuoteRate buyQuote( bank, Quote::BUY, buyRate );
+        BankQuoteRate buyQuote( bank, BuyOrSell::BUY, buyRate );
         double sellRate = atof( record[ IncomingQuoteRecord::SELLRATE ].c_str() );
-        BankQuoteRate sellQuote( bank, Quote::SELL, sellRate );
+        BankQuoteRate sellQuote( bank, BuyOrSell::SELL, sellRate );
 
+        // Step 2 -- For the future: It is imperative that function be executed before UpdateBestQuote()
         RecordBankQuotes( latestBankQuotes, bank, ccyPair, buyRate, sellRate );
-        UpdateQuote( highestBuyRates, ccyPair, buyQuote, SelectionStrategy::HIGHEST );
-        UpdateQuote( lowestSellRates, ccyPair, sellQuote, SelectionStrategy::LOWEST );
 
-        std::cout << "Best " << ccyPair << " Buy = " << highestBuyRates[ ccyPair ].price << " from " <<
-        highestBuyRates[ ccyPair ].bank << std::endl;
-        std::cout << "Best " << ccyPair << " Sell = " << lowestSellRates[ ccyPair ].price << " from " <<
-        lowestSellRates[ ccyPair ].bank << std::endl;
+        // Step 3
+        auto &bestBuyQuote  = UpdateQuote( highestBuyRates, ccyPair, buyQuote, SelectionStrategy::HIGHEST );
+        auto &bestSellQuote = UpdateQuote( lowestSellRates, ccyPair, sellQuote, SelectionStrategy::LOWEST );
+
+        // Step 4
+        std::cout << "Best " << ccyPair << " Buy = " << bestBuyQuote.price << " from " << bestBuyQuote.bank << std::endl;
+        std::cout << "Best " << ccyPair << " Sell = " << bestSellQuote.price << " from " << bestSellQuote.bank << std::endl;
     }
 
 private:
+
+    // ------- Private Enumerations/Type Definitions --------
+    enum BuyOrSell { BUY, SELL };
+    using Bank    = std::string;
+    using CcyPair = std::string;
+
+    struct BankQuoteRate {
+        Bank bank;
+        BuyOrSell buyOrSell;
+        double price;
+
+        BankQuoteRate() = default;
+        BankQuoteRate( const Bank &b, const BuyOrSell &bos, const double &p ) : bank( b ), buyOrSell( bos ), price( p ) { }
+    };
+    using BestCcyQuotes = std::map< CcyPair, BankQuoteRate >;
+    // ------ END Private Enumerations/Type Definitions -----
+
+    // Used to keep track of the best Buy Rates. Pls refer to step 3a from above
     BestCcyQuotes highestBuyRates;
+    // Used to keep track of the best Sell Rates. Pls refer to step 3b from above
     BestCcyQuotes lowestSellRates;
+
+    // ------- Private Enumerations/Type Definitions --------
+    struct CcyPairQuote {
+        CcyPair pair;
+        double buyRate;
+        double sellRate;
+
+        CcyPairQuote() = default;
+        CcyPairQuote( const CcyPair &cp, const double &bR, const double &sR ) : pair( cp ), buyRate( bR ), sellRate( sR ) { }
+    };
+    using LatestBankQuotes = std::map< Bank, std::map< CcyPair, CcyPairQuote > >;
+    // ------ END Private Enumerations/Type Definitions -----
+
+    // Used to keep track of every bank's latest rates for a currency pair. Pls refer to step 2 from above
     LatestBankQuotes latestBankQuotes;
 
-    void RecordBankQuotes( LatestBankQuotes &bankQuotes, const Bank &bank, const CcyPair &ccyPair,
-                           const double &buyRate, const double &sellRate ) {
+    /*
+     * Helper function to keep the latestBankQuotes refreshed/populated
+     */
+
+    void RecordBankQuotes( LatestBankQuotes &bankQuotes, const Bank &bank, const CcyPair &ccyPair, const double &buyRate, const double &sellRate ) {
         auto foundBank = bankQuotes.find( bank );
         if( foundBank == bankQuotes.end() ) {
             std::map< CcyPair, CcyPairQuote > tmp{ { std::make_pair( ccyPair, CcyPairQuote( ccyPair, buyRate, sellRate ) ) } };
@@ -92,7 +124,11 @@ private:
         HIGHEST, LOWEST
     };
 
-    const BankQuoteRate GetBestQuoteFromHistory( const CcyPair &ccyPair, const Quote &quote, const SelectionStrategy &strategy ) {
+    /*
+     * Helper function that look @ latest rates submitted by every bank for a particular currency pair
+     * Returns: Depending on what we are looking for, the best Buy or Sell rate
+     */
+    const BankQuoteRate GetBestQuoteFromHistory( const CcyPair &ccyPair, const BuyOrSell &quote, const SelectionStrategy &strategy ) {
         auto bankCcyPairItr = this->latestBankQuotes.begin();
         auto bestRate = ( strategy == SelectionStrategy::HIGHEST ? std::numeric_limits< double >::min() : std::numeric_limits< double >::max() );
         Bank bank;
@@ -102,13 +138,13 @@ private:
                 continue;
             }
             switch( quote ) {
-                case Quote::BUY:
+                case BuyOrSell::BUY:
                     if( bestRate < ccyPairFound->second.buyRate ) {
                         bestRate = ccyPairFound->second.buyRate;
                         bank = bankCcyPairItr->first;
                     }
                     break;
-                case Quote::SELL:
+                case BuyOrSell::SELL:
                     if( bestRate > ccyPairFound->second.sellRate ){
                         bestRate = ccyPairFound->second.sellRate;
                         bank = bankCcyPairItr->first;
@@ -116,20 +152,23 @@ private:
                     break;
             }
         }
+        // Defensive Error Handling missing
+        // This will be required in case order of function invocation is changed in ProcessRecord()
         return( BankQuoteRate( bank, quote, bestRate ) );
     }
 
-    const BankQuoteRate &UpdateQuote( BestCcyQuotes &quotes, const CcyPair &ccyPair, const BankQuoteRate &quote,
-                                  const SelectionStrategy &strategy ) {
+    /*
+     * Helper function to maintain, both, the best Buy & Sell rates
+     * Returns: a reference to the best rate for a given CCY Pair & BuyOrSell
+     */
+    const BankQuoteRate& UpdateQuote( BestCcyQuotes &quotes, const CcyPair &ccyPair, const BankQuoteRate &quote, const SelectionStrategy &strategy ) {
         auto foundQuote = quotes.find( ccyPair );
         if( foundQuote == quotes.end() ) {
             quotes.insert( std::make_pair( ccyPair, quote ) );
-            // We now want to make sure there isn't a better quote
         }
         else {
             if( foundQuote->second.bank == quote.bank ) {
-                BankQuoteRate bqr = GetBestQuoteFromHistory( ccyPair, quote.quote, strategy );
-                quotes[ ccyPair ] = bqr;
+                quotes[ ccyPair ] = GetBestQuoteFromHistory( ccyPair, quote.buyOrSell, strategy );
             }
             else {
                 switch( strategy ) {
@@ -144,8 +183,7 @@ private:
                 }
             }
         }
-
-        return ( quotes[ ccyPair ] );
+        return quotes[ ccyPair ];
     }
 };
 
@@ -171,6 +209,20 @@ int main( int argc, char *argv[] ) {
             quoteManager.ProcessRecord( quote );
         }
     }
+
+    /*
+     * FUTURE Improvements that will/may be required:
+     * 1. Error/Exception Handling - This WILL be required once we determine strategy to handle bad records/missing data
+     * 2. Thread Safety - The QuoteManager's internal data structures are not locked while being updated.
+     *                    Inconsistent reads are likely to occur in a multi-threaded environment
+     * 3. Efficiency - After profiling with a larger production sample, we could
+     *                 a) Use custom allocators if we are spending most time in memory allocation
+     *                 b) Choose an alternate implementation for maintaining QuoteManager's state. Possibly an
+     *                      implementation that keeps track of input records and provides abstract interface from a
+     *                      Currency Pair or Bank point of view when required
+     * 4. QuoteManager should probably be a singleton or at least be able to share its data structures if required
+     *      in other parts of the program. All of the QuoteManager's internal state can be moved to a separate class
+     */
 
     return 0;
 }
